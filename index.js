9f0d794c320ebd602db036a13b2249733aaa4a72
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-26 21:11:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,8 +43,14 @@ Polygon.prototype.perimeter = function() {
   return rim ;
 }
 
-function Quadrilateral(s1, s2, s3,s4) {
+function Quadrilateral(s1, s2, s3, s4) {
   this.sides = [new Side(s1), new Side(s2), new Side(s3), new Side(s4)] ;
 }
 Quadrilateral.prototype = Object.create(Polygon.prototype) ;
 Quadrilateral.prototype.constructor = Polygon ;
+
+function Triangle(s1, s2, s3) {
+  this.sides = [new Side(s1), new Side(s2), new Side(s3)] ;
+}
+Triangle.prototype = Object.create(Polygon.prototype) ;
+Triangle.prototype.constructor = Polygon ;
