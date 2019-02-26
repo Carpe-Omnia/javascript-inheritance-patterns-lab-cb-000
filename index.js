860514319f8b9454c46c0ef661034084b5b3b888
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-26 20:31:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function Point(x,y) {
     return `(${this.x},${this.y})` ;
   }
 }
+
 function Shape() {
   this.addToPlane = function(x,y) {
     this.position = Point(x,y) ;
@@ -13,9 +14,11 @@ function Shape() {
     this.position = Point(x,y)
   }
 }
+
 function Circle(radius) {
   this.radius = radius ;
   this.area = radius^2 * 3.14159 ;
   this.circumference = radius * 2 * 3.14159 ;
 }
 Circle.prototype = Object.create(Shape.prototype) ;
+Circle.prototype.constructor = Shape ;
