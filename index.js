c57014b3b35fbd5e014a05513b09beda697c8236
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-26 21:29:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ Square.prototype.listProperties =  function() {
   var stuff = "" ;
   for (var prop in sq) {
     if (sq.hasOwnProperty(prop)) {
-      stuff.push( prop )  ;
+      stuff += prop ; 
     }
     return stuff ;
   }
